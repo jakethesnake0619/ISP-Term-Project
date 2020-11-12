23b30e50fedea4162e5d291792b8171907ab6438
jakethesnake0619 <59973256+jakethesnake0619@users.noreply.github.com>
2020-11-12 13:22:18 -0500

    Update main.php

--- main.php
+++ main.php
@@ -5,9 +5,41 @@
 <body>
 <!--All items are displayed here-->
 
-<h1> All items are here!</h1>
+<?php
+	$db = mysqli_connect("localhost:3306", "root", "", "isp");
 
+	$mainpage = mysqli_query($db, "SELECT id FROM mainpage ORDER BY id");
+	while($row = $mainpage->fetch_assoc()) {
+		if(isset($_GET[$row["id"]])) {
+			mysqli_query($db, "UPDATE cart SET quantity = quantity + 1 WHERE id = " . $row["id"] . ";");
+			mysqli_query($db, "UPDATE mainpage SET quantity = quantity - 1 WHERE id = " . $row["id"] . ";");
+		}
+	}
 
+	$mainpage = mysqli_query($db, "SELECT * FROM mainpage ORDER BY id");
+	print '<form action = "http://localhost/isp/prj/prj.php" method = "get">';
+	print "<table class = 'center'>";
+	?>
+        <th> Product Name </th>
+        <th> Price </th>
+        <th> Quantity </th>
+        <th> Add To Cart </th>
+	<?php
+	while($row = $mainpage->fetch_assoc()) {
+		if($row["quantity"] > 0) {
+			print "<tr>";
+			print "<td>" . $row["name"] . "</td>";
+			print "<td>" . "$" . $row["price"] . "</td>";
+			print "<td>" . $row["quantity"] . "</td>";
+			print "<td>"; 
+			print '<div id = "add"><input type = "submit" value = "Add To Cart" name = "' . $row["id"] . '"/> </div>';
+			print " </td>";
+			print "</tr>";
+		}
+	}
+	print "</table>";
+	print "</form>";
+?>
 
 </body>
 
