1503c46a8da2e19db4ff69b9e6a0a6be589299c9
michael ameteku <Ameteku@github.com>
2020-11-17 12:48:06 -0500

    Push user info provided to db and send user to confirmation page

--- checkout.php
+++ checkout.php
@@ -9,29 +9,55 @@
     foreach($results as $item){
         $tot += $item['price'] * $item['quantity'];
     };
+
+    if(isset($_POST['submit']))
+    {
+        $first = mysqli_real_escape_string($db,$_POST['fname']);
+        $second =mysqli_real_escape_string($db, $_POST['lname']);
+        $email = mysqli_real_escape_string($db,$_POST['email']);
+        $addy = mysqli_real_escape_string($db,$_POST['address']);
+        $city =mysqli_real_escape_string($db,$_POST['city']);
+        $state = mysqli_real_escape_string($db,$_POST['state']);
+        $zipcode = mysqli_real_escape_string($db,$_POST['zipcode']);
+        $phone = mysqli_real_escape_string($db,$_POST['phone']);
+
+    $push = "INSERT INTO users(fname,lname,email,addy,city,Stat,zipcode,phone) VALUES
+     ('$first', '$second','$email','$addy','$city','$state','$zipcode','$phone')";
     
+    if(mysqli_query($db,$push))
+    {
+        header('Location : confirmation.php');
+    }
+    else
+    {
+        echo "query error".mysqli_error($db);
+    }; 
+    
+
+    };
+        
 ?>
 <P class = "Userinfo" id= "heading">Getting your Order</P>
 
 
-<form action= "checkout.php" method="POST" id = "user_info">
+<form action= "checkout.php" method="POST" id ="user_info">
     <nav>
         <label>First name</label><br/>
-        <input  class ="userinfo" type = "text" required><br/>
+        <input  class ="userinfo" type = "text" name='fname' required><br/>
         <label>Last name</label><br/>
-        <input class= "userinfo" type = "text" required><br/>
+        <input class= "userinfo" type = "text" name = 'lname'required><br/>
         <label>Email</label><br/>
-        <input class= "userinfo" type = "text" required><br/>
+        <input class= "userinfo" type = "text" name = 'email' required><br/>
         <label>Address</label><br/>
-        <input class= "userinfo" type = "text" required><br/>
+        <input class= "userinfo" type = "text" name = 'address' required><br/>
         <label>City</label><br/>
-        <input class= "userinfo" type = "text" required><br/>
+        <input class= "userinfo" type = "text" name = 'city' required><br/>
         <label>State</label><br/>
-        <input class= "userinfo" type = "text" required><br/>
+        <input class= "userinfo" type = "text" name = 'state' required><br/>
         <label>Zipcode</label><br/>
-        <input class= "userinfo" type = "number"  min = '10000' max = '99999' required><br/>
+        <input class= "userinfo" type = "number"  name = 'zipcode' min = '10000' max = '99999' required><br/>
         <label>Phone Number</label><br/>
-        <input class= "userinfo" type = "number" required><br/>
+        <input class= "userinfo" type = "number" name = 'phone' required><br/>
         <br/> <br/>
         <!--<table class = "summary_table">
             <h1>Summary</h1>
@@ -43,9 +69,11 @@
         </table>-->
         <label class = "summary"> Total</label>
         <input type="number" value =<?php echo $tot; ?>>
+        <input type = "submit" name = "submit" value = "Check out">
     </nav>
 </form>
-<input type = "submit" value = "Check out">
+
+
 
 
 
---
+++ confirmation.php
@@ -0,0 +1,11 @@
+<?php include "header.php"?>
+<?php
+
+$conn = mysqli_connect("localhost", "Mike", "moneyMaker1", "isp_db");
+
+
+
+
+
+
+?>
