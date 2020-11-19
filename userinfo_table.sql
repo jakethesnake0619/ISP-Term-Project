481912a623d17269a9b756ae578dd1d5a38d3878
michael ameteku <Ameteku@github.com>
2020-11-18 21:45:08 -0500

    Update sql creation to take appropriate values

--- userinfo_table.sql
+++ userinfo_table.sql
@@ -1,15 +1,15 @@
 DROP TABLE IF EXISTS Users;
 
 CREATE TABLE Users(
-	id INT(7),
+	id INT AUTO_INCREMENT  NOT NULL,
 	fname CHAR(20),
     lname CHAR(20),
 	email CHAR(30),
 	addy CHAR(30),
     city CHAR(20),
     Stat CHAR(2),
-    zipcode INT,
-    phone INT,
+    zipcode VARCHAR(5),
+    phone VARCHAR(20),
 	PRIMARY KEY (id)
 );
 
