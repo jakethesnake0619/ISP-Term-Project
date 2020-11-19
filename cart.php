acd2393c2a8ff12c7e67855c14ba32500598e792
jakethesnake0619 <59973256+jakethesnake0619@users.noreply.github.com>
2020-11-19 13:12:32 -0500

    Update cart.php

--- cart.php
+++ cart.php
@@ -1,68 +1,80 @@
+<!DOCTYPE html>
 <html>
 <head>
-  <title> Shopping Cart </title>
-  <style type = "text/css">
-  td, th, table {border: thin solid black;}
-  </style>
+    <title> Shopping Cart </title>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
 </head>
+
+<?php include "header.php"; ?>
 
 <body>
 
-<h1> Shopping Cart </h1>
+<h1 class = "carth1"> Shopping Cart </h1>
 
 <?php
-  $db = mysqli_connect("localhost", "root", "", "term2");
+    
+    $db = mysqli_connect("localhost", "root", "", "isp");
 
-  $cartpage = mysqli_query($db, "SELECT id FROM cart ORDER BY id");
-  while($row = $cartpage->fetch_assoc()) {
-    if(isset($_GET[$row["id"]])) {
-      mysqli_query($db, "UPDATE cart SET quantity = quantity - 1 WHERE id = " . $row["id"] . ";");
-      mysqli_query($db, "UPDATE main SET quantity = quantity + 1 WHERE id = " . $row["id"] . ";");
+    $cartpage = mysqli_query($db, "SELECT id FROM cart ORDER BY id");
+    while($row = $cartpage->fetch_assoc()) {
+        if(isset($_GET[$row["id"]])) {
+            mysqli_query($db, "UPDATE cart SET quantity = quantity - 1 WHERE id = " . $row["id"] . ";");
+            mysqli_query($db, "UPDATE mainpage SET quantity = quantity + 1 WHERE id = " . $row["id"] . ";");
+            header("Location: http://localhost/isp/prj/cart.php");
+        }
     }
-  }
-
-  $cartpage = mysqli_query($db, "SELECT * FROM cart ORDER BY ID");
-  print '<form action = "http://localhost/isp/cart.php" method = "get">';
-  print "<table class = 'center'>";
-  ?>
-  <th> Product Name </th>
-  <th> Price </th>
-  <th> Quantity </th>
-  <th> Action </th>
-
-  <?php
-  while($row = $cartpage->fetch_assoc()) {
-    if($row["quantity"] > 0) {
-      print "<tr>";
-      print "<td>" . $row["name"] . "</td>";
-      print "<td>" . "$" . $row["price"] . "</td>";
-      print "<td>" . $row["quantity"] . "</td>";
-      print "<td>";
-      print '<div id = "sub"><input type = "submit" value = "Remove From Cart" name = "' . $row["id"] . '"/> </div>';
-      print " </td>";
-      print "</tr>";
-    }
-  }
-
-
 
     $cartpage = mysqli_query($db, "SELECT * FROM cart ORDER BY ID");
-    print '<div id = "all"><input type = "submit" value = "Remove All" name = "all"> </div>';
-    if(isset($_GET["all"])) {
-      while($row = $cartpage->fetch_assoc()) {
-        $quant = $row['quantity'];
-          mysqli_query($db, "UPDATE cart SET quantity = quantity - $quant WHERE id =" . $row["id"] . ";");
-          mysqli_query($db, "UPDATE main SET quantity = quantity + $quant WHERE id =" . $row["id"] . ";");
-        }
-        header("Location: http://localhost/isp/cart.php");
-    }
-    print "</table";
-    print "</form>";
+    print '<form action = "http://localhost/isp/prj/cart.php" method = "get">';
+    print "<table id = 'carttable'>";
     ?>
 
+    <th> Product Name </th>
+    <th> Description </th>
+    <th> Price </th>
+    <th> Quantity </th>
+    <th> Action </th>
 
+    <?php
+    while($row = $cartpage->fetch_assoc()) {
+        if($row["quantity"] > 0) {
+            print "<tr>";
+            print "<td>" . $row["name"] . "</td>";
+            print "<td>";
+            print "<div class='description'> <span>Hover To View</span><div class='description-content'><p>" . $row["description"] . "</p></div></div>";
+            print "</td>";
+            print "<td>" . "$" . $row["price"] . "</td>";
+            print "<td>" . $row["quantity"] . "</td>";
+            print "<td>";
+            print "<input type = 'submit' class = 'cartlist' value = 'Remove From Cart' name = '" . $row["id"] . "'/>";
+            print " </td>";
+            print "</tr>";
+        }
+    }
+
+    print "</table>";
+
+    $cartpage = mysqli_query($db, "SELECT * FROM cart ORDER BY ID");
+    print '<input type = "submit" value = "Remove All" name = "all">';
+    if(isset($_GET["all"])) {
+        while($row = $cartpage->fetch_assoc()) {
+            mysqli_query($db, "UPDATE mainpage SET quantity = quantity + " . $row['quantity'] . " WHERE id =" . $row["id"] . ";");
+            mysqli_query($db, "UPDATE cart SET quantity = 0 WHERE id =" . $row["id"] . ";");
+        }
+        header("Location: http://localhost/isp/prj/cart.php");
+    }    
+    print "</form>";
+?>
+<br>
+
+<form action = "http://localhost/isp/prj/checkout.php" method = "get">
+    <input type = "submit" value = "Checkout"/>
+</form>
+
+<br/><br/><br/>
+
+<?php include "footer.php"?>
 
 </body>
 </html>
-
 
