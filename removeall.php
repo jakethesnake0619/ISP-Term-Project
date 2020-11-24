4947926ca73ee15b51fbe1bbe417f184de02bb18
NickMiller32 <74265345+NickMiller32@users.noreply.github.com>
2020-11-24 18:38:54 -0500

    Create removeall.php

---
+++ removeall.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Are you sure?</title>
+	<link href="style.css" type="text/css" rel="stylesheet"/>
+</head>
+
+<?php include "header.php"?>
+
+<body>
+<?php
+  print '<form action = "http://localhost/isp/removeall.php" method = "get">';
+?>
+<br/>
+<br/>
+<h1> Are you sure you want to remove all items? </h1>
+<br/>
+<br/>
+<?php
+print '<input type = "submit" class = "cartlist" value = "Cancel" name = "cancel">';
+?>
+<br/>
+<br/>
+<br/>
+<?php
+ print '<input type = "submit" class = "removeAll" value = "Remove All" name = "remove">';
+
+$db = mysqli_connect("localhost", "root", "", "term2");
+$cartpage = mysqli_query($db, "SELECT * FROM cart ORDER BY ID");
+if(isset($_GET["cancel"])) {
+    header("Location: http://localhost/isp/cart2.php");
+}
+
+if(isset($_GET["remove"])) {
+  while($row = $cartpage->fetch_assoc()) {
+    mysqli_query($db, "UPDATE mainpage SET quantity = quantity + " . $row['quantity'] . " WHERE id =" . $row["id"] . ";");
+    mysqli_query($db, "UPDATE cart SET quantity = 0 WHERE id =" . $row["id"] . ";");
+
+}
+header("Location: http://localhost/isp/cart2.php");
+}
+?>
+
+<br/><br/><br/>
+<?php include "footer.php"?>
+
+</body>
+</html>
