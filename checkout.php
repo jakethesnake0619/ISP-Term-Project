246d564a446ea6c6daf805aed471ee60ab4df671
jakethesnake0619 <59973256+jakethesnake0619@users.noreply.github.com>
2020-11-24 19:08:55 -0500

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -1,15 +1,19 @@
+<head>
+    <title>Checkout</title>
+    <link href="style.css" type="text/css" rel="stylesheet"/>
+</head>
+
 <?php include "header.php"?>
 
-<?php $db = mysqli_connect("localhost", "Mike", "moneyMaker1", "isp_db");
-
+<?php 
+    $db = mysqli_connect("localhost:3306", "root", "", "isp");
     $items = mysqli_query($db, "SELECT * FROM cart WHERE quantity!=0");
-
     $results = mysqli_fetch_all($items, MYSQLI_ASSOC);
     $tot=0;
-    foreach($results as $item){
+    foreach($results as $item)
+    {
         $tot += $item['price'] * $item['quantity'];
-    };
-
+    }
     if(isset($_POST['submit']))
     {
         $first = mysqli_real_escape_string($db,$_POST['fname']);
@@ -22,27 +26,22 @@
         $phone = mysqli_real_escape_string($db,$_POST['phone']);
 
     $push = "INSERT INTO users(fname,lname,email,addy,city,Stat,zipcode,phone) VALUES
-     ('$first', '$second','$email','$addy','$city','$state','$zipcode','$phone')";
-    
-    if(mysqli_query($db,$push))
-    {
-        $removeq = "UPDATE cart SET quantity =0;";
-        mysqli_query($db,$removeq);
-        header('Location: confirmation.php');
-    }
-    else
-    {
-        echo "query error".mysqli_error($db);
-    }; 
-    
-
-    };
-        
+    ('$first', '$second','$email','$addy','$city','$state','$zipcode','$phone')";
+        if(mysqli_query($db,$push))
+        {
+            $removeq = "UPDATE cart SET quantity =0;";
+            mysqli_query($db,$removeq);
+            header('Location: confirmation.php');
+        }
+        else
+        {
+            echo "query error".mysqli_error($db);
+        } 
+    }       
 ?>
-<P class = "Userinfo" id= "heading">Getting your Order</P>
-
-
-  <div class = "salesum">
+<body>
+    <p class = "center" id= "heading">Order Checkout</p>
+    <div class = "salesum">
         <div id = "user-info">
             <form action= "checkout.php" method="POST" id = "checkoutform">
                 <label>First name</label><br/>
@@ -61,22 +60,21 @@
                 <input class= "userinfo" type = "number"  name = 'zipcode' min = '10000' max = '99999' required><br/>
                 <label>Phone Number</label><br/>
                 <input class= "userinfo" type = "text" name = 'phone' required><br/>
-                
+                <br/>
                 <input type = "submit" name = "submit" value = "Check out" >
                 <br/>
             </form>
         </div>
-         <div id = "summaryt">
+        <div id = "summaryt">
             <h1>Summary</h1>
             <table class = "summary_table">
                 <tr>
                 <th>Product</th>
-                <th>Qty</th>
+                <th>Quantity</th>
                 <th>Subtotal</th>
                 </tr>
-                <?php  if(sizeof($results)!=0):?>
+                    <?php  if(sizeof($results)!=0):?>
                     <?php foreach($results as $item):?>
-                        
                         <tr> 
                             <td><?php echo $item['name'];?></td>
                             <td><?php echo 'x'.$item['quantity'];?></td>
@@ -84,20 +82,17 @@
                         </tr>
                     <?php endforeach ?>  
                     <?php else:?>
-                        <td>Empty Cart</td>
-                    <?php endif;?>
-                
+                        <td colspan="3">Empty Cart</td>
+                    <?php endif;?>     
                 <tr>
                     <th>Total</th>
                     <hr/>
-                    <td style = "text-align: right"><?php echo $tot ?><td>
+                    <td colspan="2"><?php echo "$" . $tot; ?></td>
                  </tr>          
-            </table>
-            
+            </table> 
         </div>
     </div>
 </body>
-
 
 
 
