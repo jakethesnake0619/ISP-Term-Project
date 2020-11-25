8bc614eacc91963a9b4ef7fbd9743423962ed0a3
jakethesnake0619 <59973256+jakethesnake0619@users.noreply.github.com>
2020-11-24 19:10:14 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,2 +1,7 @@
-<p>Copyright@ the Shop Owners</p>
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="style.css" type="text/css" rel="stylesheet"/>
+</head>
+	<p class = "footer">Copyright@ nozamA</p>
 </html>
